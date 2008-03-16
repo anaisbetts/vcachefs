f7de5f084be552e362364d5eab9b2c670dcf0c67
Paul Betts <paul@paulbetts.org>
2008-03-15 22:10:44 -0700

    Correct README file, set up basic structures and stub out functions we'll need to implement

--- src/vcachefs.c
+++ src/vcachefs.c
@@ -21,7 +21,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
-
 #include <fuse.h>
 #include <stdio.h>
 #include <string.h>
@@ -29,83 +28,125 @@
 #include <fcntl.h>
 #include <glib.h>
 
-static const char *hello_str = "Hello World!\n";
-static const char *hello_path = "/hello";
+#include "vcachefs.h"
 
-static int hello_getattr(const char *path, struct stat *stbuf)
+static void* vcachefs_init(struct fuse_conn_info *conn)
 {
-    int res = 0;
-
-    memset(stbuf, 0, sizeof(struct stat));
-    if(strcmp(path, "/") == 0) {
-        stbuf->st_mode = S_IFDIR | 0755;
-        stbuf->st_nlink = 2;
-    }
-    else if(strcmp(path, hello_path) == 0) {
-        stbuf->st_mode = S_IFREG | 0444;
-        stbuf->st_nlink = 1;
-        stbuf->st_size = strlen(hello_str);
-    }
-    else
-        res = -ENOENT;
-
-    return res;
+	// TODO: Implement me
 }
 
-static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-                         off_t offset, struct fuse_file_info *fi)
+static void vcachefs_destroy(void *mount_object)
 {
-    (void) offset;
-    (void) fi;
-
-    if(strcmp(path, "/") != 0)
-        return -ENOENT;
-
-    filler(buf, ".", NULL, 0);
-    filler(buf, "..", NULL, 0);
-    filler(buf, hello_path + 1, NULL, 0);
-
-    return 0;
+	// TODO: Implement me
 }
 
-static int hello_open(const char *path, struct fuse_file_info *fi)
+static int vcachefs_getattr(const char *path, struct stat *stbuf)
 {
-    if(strcmp(path, hello_path) != 0)
-        return -ENOENT;
+	int res = 0;
 
-    if((fi->flags & 3) != O_RDONLY)
-        return -EACCES;
+	memset(stbuf, 0, sizeof(struct stat));
+	if(strcmp(path, "/") == 0) {
+		stbuf->st_mode = S_IFDIR | 0755;
+		stbuf->st_nlink = 2;
+	}
+	else if(strcmp(path, vcachefs_path) == 0) {
+		stbuf->st_mode = S_IFREG | 0444;
+		stbuf->st_nlink = 1;
+		stbuf->st_size = strlen(vcachefs_str);
+	}
+	else
+		res = -ENOENT;
 
-    return 0;
+	return res;
 }
 
-static int hello_read(const char *path, char *buf, size_t size, off_t offset,
-                      struct fuse_file_info *fi)
+static int vcachefs_open(const char *path, struct fuse_file_info *fi)
 {
-    size_t len;
-    (void) fi;
-    if(strcmp(path, hello_path) != 0)
-        return -ENOENT;
+	if(strcmp(path, vcachefs_path) != 0)
+		return -ENOENT;
 
-    len = strlen(hello_str);
-    if (offset < len) {
-        if (offset + size > len)
-            size = len - offset;
-        memcpy(buf, hello_str + offset, size);
-    } else
-        size = 0;
+	if((fi->flags & 3) != O_RDONLY)
+		return -EACCES;
 
-    return size;
+	return 0;
 }
 
-static struct fuse_operations hello_oper = {
-    .getattr	= hello_getattr,
-    .readdir	= hello_readdir,
-    .open	= hello_open,
-    .read	= hello_read,
+static int vcachefs_read(const char *path, char *buf, size_t size, off_t offset,
+		struct fuse_file_info *fi)
+{
+	size_t len;
+	(void) fi;
+	if(strcmp(path, vcachefs_path) != 0)
+		return -ENOENT;
+
+	len = strlen(vcachefs_str);
+	if (offset < len) {
+		if (offset + size > len)
+			size = len - offset;
+		memcpy(buf, vcachefs_str + offset, size);
+	} else
+		size = 0;
+
+	return size;
+}
+
+
+static int vcachefs_statfs(const char *path, struct statvfs *stat)
+{
+	// TODO: Implement me
+}
+
+
+static int vcachefs_release(const char *path, struct fuse_file_info *info)
+{
+	// TODO: Implement me
+}
+
+
+static int vcachefs_access(const char *path, int amode)
+{
+	// TODO: Implement me
+}
+
+static int vcachefs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+		off_t offset, struct fuse_file_info *fi)
+{
+	(void) offset;
+	(void) fi;
+
+	if(strcmp(path, "/") != 0)
+		return -ENOENT;
+
+	filler(buf, ".", NULL, 0);
+	filler(buf, "..", NULL, 0);
+	filler(buf, vcachefs_path + 1, NULL, 0);
+
+	return 0;
+}
+
+
+static struct fuse_operations vcachefs_oper = {
+	.getattr	= vcachefs_getattr,
+	/*.readlink 	= vcachefs_readlink, */
+	.open 		= vcachefs_open,
+	.read		= vcachefs_read,
+	.statfs 	= vcachefs_statfs,
+	/* TODO: do we need flush? */
+	.release 	= vcachefs_release,
+	.init 		= vcachefs_init,
+	.destroy 	= vcachefs_destroy,
+	.access 	= vcachefs_access,
+
+	/* TODO: implement these later
+	.getxattr 	= vcachefs_getxattr,
+	.listxattr 	= vcachefs_listxattr,
+	.opendir 	= vcachefs_opendir, */
+	.readdir	= vcachefs_readdir,
+	/*.releasedir 	= vcachefs_releasedir,
+	.fsyncdir 	= vcachefs_fsyncdir, */
 };
 
 int main(int argc, char *argv[])
 {
-    return fuse_main(argc, argv, &hello_oper, NULL);
+	return fuse_main(argc, argv, &vcachefs_oper, NULL);
 }
---
+++ src/vcachefs.h
@@ -0,0 +1,48 @@
+/*
+ * vcachefs.h - Userspace video caching filesystem 
+ *
+ * Copyright 2008 Paul Betts <paul.betts@gmail.com>
+ *
+ *
+ * License:
+ *
+ * This package is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this package; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef _VCACHEFS_H
+#define _VCACHEFS_H
+
+/* This object is the per-mount data we carry around with us throughout the 
+ * life of the app until we release it */
+struct vcachefs_mount {
+	const char* 	source_path,	
+	
+	/* File descriptor table */
+	GHashTable* 	fd_table,
+	GStaticRWLock 	fd_table_rwlock,
+};
+
+struct vcachefs_fdentry {
+	int fd,
+	int source_fd,
+};
+
+
+/* Function declarations */
+
+static struct vcachefs_fdentry* fdentry_new(void);
+static fdentry_free(struct vcachefs_fdentry* obj);
+
+#endif 
