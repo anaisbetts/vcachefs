782d1e2f70b693afd57154d560ca015651cf3ed2
Paul Betts <paul@paulbetts.org>
2008-03-15 21:18:40 -0700

    Credit where credit's due!

--- src/vcachefs.c
+++ src/vcachefs.c
@@ -1,10 +1,26 @@
 /*
-    FUSE: Filesystem in Userspace
-    Copyright (C) 2001-2005  Miklos Szeredi <miklos@szeredi.hu>
+ * vcachefs.c - Userspace video caching filesystem 
+ *
+ * Copyright 2008 Paul Betts <paul.betts@gmail.com>
+ *
+ *
+ * License:
+ *
+ * This package is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this package; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
 
-    This program can be distributed under the terms of the GNU GPL.
-    See the file COPYING.
-*/
 
 #include <fuse.h>
 #include <stdio.h>
